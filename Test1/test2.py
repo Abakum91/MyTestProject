3ec5fa924caf89098a6fbd34ff2a81e95f7633af
Alexey Abakumov <ramzi91@yandex.ru>
2021-07-23 17:05:31 +0300

    work

---
+++ Test1/test2.py
@@ -0,0 +1,49 @@
+#name = input('Enter your name')
+#print('Hello', name)
+
+#a = int(input())
+#print(a*2)
+
+#a = int(input())
+#b = int(input())
+#print(a * b)
+
+#X = int(input()) задачка про сон Х часов и У минут
+#Y = int(input())
+#print(X*60 + Y)
+
+#X = int(input())
+#print(int(X // 60))
+#print(int(X % 60))
+
+# or - или, and - и, not - унарная, по приоритету выполнения - not, and, or
+# AND - умножение, таким образом: True*False (1*0) = False, умножение на 0 всегда дает 0
+# OR - сложение, таким образом: True+False (1+0) = True, в случае сложения True+True(1+1) - всегда будет True
+
+#x = int(input())                                      #условия
+#if x%2 == 0:
+#    print('Четное')
+#else:
+#    print("Нечетное")
+
+#a = int(input())
+#b = int(input())
+#if b != 0:
+#    print(a / b)
+#else:
+#    print('Деление невозможно')
+#    b = int(input('Введите не нулевое значение, '))
+#    if b == 0:
+#        print('Вы не справились')
+#    else:
+#        print(a / b)
+
+#a, b, h = [int(input()) for i in range(3)]
+#if h >= a and h <= b:
+#    print ('Это нормально')
+#else:
+#    if h < a:
+#        print ('Недосып')
+#    if h > b:
+#        print ('Пересып')
+
